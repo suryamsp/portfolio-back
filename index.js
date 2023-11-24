982e485a7b883d63d5c82cbe0d5880c8c3ec17d2
suryamsp <murugesansuryamsp@gmail.com>
2023-11-24 06:26:47 -0800

    port-beckend

---
+++ index.js
@@ -0,0 +1,34 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config()
+
+import express from 'express';
+const app = express();
+import { MongoClient } from "mongodb";
+import cors from "cors";
+
+
+
+
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT; 
+const mongo_url = process.env.mongo_url;
+export const client = new MongoClient(mongo_url);
+console.log("mongodb is connected ");
+
+
+
+app.get("/",async function (request, response) {
+        const store= await client
+        .db("portfolio")
+        .collection("resume")
+        .find({})
+        .toArray([])
+        response.send(store);
+});
+
+
+
+app.listen(PORT,()=> console.log(`server started : ${PORT}`));
